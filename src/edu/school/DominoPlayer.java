962a19f898895ca7f0f7a22894f75dcf01d85909
Asrock <esvalcheva@gmail.com>
2020-12-10 17:04:04 +0200

    domino

--- src/edu/school/DominoPlayer.java
+++ src/edu/school/DominoPlayer.java
@@ -1,5 +1,74 @@
 package edu.school;
 
 public class DominoPlayer {
+	
+	private String nickname; 
+	private DominoTile[] tiles; 
+	private int tilesInHands; 
+	private DominoTable table;
+	
+	public String getNickname()
+	{
+		return nickname;
+	}
+	
+	public DominoTile[] getTiles()
+	{
+		return tiles;
+	}
+	
+	public int getTilesInHands()
+	{
+		return tilesInHands;
+	}
+	
+	public DominoTable getTable()
+	{
+		return table;
+	}
+	
+	public void setNickname(String nickname) 
+	{
+		if (nickname != null && !nickname.isEmpty()) 
+		{
+			this.nickname = nickname;
+		}
+		
+	}	
+	
+	
+	public DominoPlayer(String nickname, DominoTile[] tiles, DominoTable table)
+	{
+		setNickname(nickname);
+		this.tiles = tiles;
+		this.table = table;
+		
+	}
+	
+	private int getTileIndex(DominoTile t)
+	{
+		for(int i = 0 ; i < tiles.length ; i++)
+		{
+			if(tiles[i] == t)
+			{
+				return i;
+			}
+		}
+		return -1;
+		
+	}
+	
+	public void addTileAtLeft(DominoTile t)
+	{
+		
+	}
+	
+
+
+
+
 
 }
+
+
+
